6b39754e54dc85c4a41318c06b6d64f5b020e07d
Asif Ishtiaq <ishtiaqasif03@gmail.com>
2025-07-07 20:05:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['firebase', '@firebase/auth', '@firebase/firestore'],
   eslint: {
     ignoreDuringBuilds: true,
   },
